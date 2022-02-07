6856b85f49cc1fed70d1523d8e8f917a8362d4fc
Bassel <bassel444555@gmail.com>
2022-02-07 14:48:52 +0200

    end

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+let icon = document.querySelector('.icon'),
+  nav = document.querySelector('nav'),
+  nav_ul_li = document.querySelectorAll('nav>ul>li');
+
+icon.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  icon.classList.toggle('active');
+  nav_ul_li.forEach((l) => l.classList.remove('active'));
+});
+
+nav_ul_li.forEach((li) => {
+  li.addEventListener('click', function(e) {
+    if (!li.classList.contains('active'))
+      nav_ul_li.forEach((l) => l.classList.remove('active'));
+    this.classList.toggle('active');
+  });
+});
+
+
+
+
+
 let meida = window.matchMedia("(max-width: 767px)");
 let img = document.querySelector('.future img');
 let img2 = document.querySelector('.featur img');
